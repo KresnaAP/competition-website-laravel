e837713106d0b491109d49547564be812ca117e4
KresnaAP <kresnaadhipramana5758@gmail.com>
2020-07-11 12:51:02 +0700

    adding argon style to create and edit user page

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,21 +2,48 @@
 <html>
 <head>
 	<title>Add User</title>
+	<!-- Favicon -->
+	<link href="{{ asset('argon') }}/img/brand/favicon.png" rel="icon" type="image/png">
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+	<!-- Icons -->
+	<link href="{{ asset('argon') }}/vendor/nucleo/css/nucleo.css" rel="stylesheet">
+	<link href="{{ asset('argon') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+	<!-- Argon CSS -->
+	<link type="text/css" href="{{ asset('argon') }}/css/argon.css?v=1.0.0" rel="stylesheet">
 </head>
 <body>
 	<h3>Add User</h3>
 
-	<a href="/user">Back</a>
+	<a href="/user" class="btn btn-primary">Back</a>
 	
 	<br/>
 	<br/>
 
 	<form action="/user/store" method="post">
 		@csrf
-		Name <input type="text" required="required" name="name" placeholder="Name"> <br/>
-		Email <input type="email" required="required" name="email" placeholder="Email"> <br/>
-		Password <input type="password" required="required" name="password" placeholder="Password"> <br/>
-    <button type="submit" class="btn btn-success mt-4">{{ __('Save') }}</button>
+		<div class="row">
+    		<div class="col-md-4">
+      			<div class="form-group">
+				  	Name <input type="text" class="form-control form-control-alternative" required="required" name="name" placeholder="Name"> <br/>
+      			</div>
+    		</div>
+		</div>
+		<div class="row">
+    		<div class="col-md-4">
+      			<div class="form-group">
+				  	Email <input type="email" class="form-control form-control-alternative" required="required" name="email" placeholder="Email"> <br/>
+				</div>
+    		</div>
+		</div>
+		<div class="row">
+    		<div class="col-md-4">
+      			<div class="form-group">
+					Password <input type="password" class="form-control form-control-alternative" required="required" name="password" placeholder="Password"> <br/>
+				</div>
+    		</div>
+		</div>
+    	<button type="submit" class="btn btn-success mt-4">{{ __('Save') }}</button>
 	</form>
 
 </body>
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,25 +1,46 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Edit User</title>
+    <title>Edit User</title>
+    <!-- Favicon -->
+    <link href="{{ asset('argon') }}/img/brand/favicon.png" rel="icon" type="image/png">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <!-- Icons -->
+    <link href="{{ asset('argon') }}/vendor/nucleo/css/nucleo.css" rel="stylesheet">
+    <link href="{{ asset('argon') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <!-- Argon CSS -->
+    <link type="text/css" href="{{ asset('argon') }}/css/argon.css?v=1.0.0" rel="stylesheet">
 </head>
 <body>
-	<h3>Edit User</h3>
+    <h3>Edit User</h3>
 
-	<a href="/user">Back</a>
-	
-	<br/>
-	<br/>
+    <a href="/user" class="btn btn-primary">Back</a>
+    
+    <br/>
+    <br/>
 
-  @foreach($user as $u)
+    @foreach($user as $u)
     <form action="/user/update" method="post">
-      @csrf
-      <input type="hidden" name="id" value="{{ $u->id }}"> <br/>
-      Name <input type="text" required="required" name="name" value="{{ $u->name }}"> <br/>
-      Email <input type="email" required="required" name="email" value="{{ $u->email }}"> <br/>
-      <button type="submit" class="btn btn-success mt-4">{{ __('Save') }}</button>
+        @csrf
+        <input type="hidden" name="id" value="{{ $u->id }}"> <br/>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="form-group">
+                    Name <input type="text" class="form-control form-control-alternative" required="required" name="name" value="{{ $u->name }}"> <br/>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="form-group">
+                    Email <input type="email" class="form-control form-control-alternative" required="required" name="email" value="{{ $u->email }}"> <br/>
+                </div>
+            </div>
+        </div>
+        <button type="submit" class="btn btn-success mt-4">{{ __('Save') }}</button>
     </form>
-  @endforeach
+    @endforeach
 
 </body>
 </html>
